a8a1bcc131170a7347deb911d99224ad0a3539c8
Valentin Melito <vmelito686@gmail.com>
2023-10-23 19:08:58 -0300

    practica de codigo QR

---
+++ src/ar/edu/unlam/dominio/Producto.java
@@ -0,0 +1,62 @@
+package ar.edu.unlam.dominio;
+
+public class Producto {
+	
+	/***
+	 * Constructor de la clase
+	 * @param codigo
+	 * @param precio
+	 * @param descripcion
+	 */
+	public Producto(int codigo, double precio, String descripcion) {
+		
+	}
+	
+	/****
+	 * Devuelve el c�digo de un producto
+	 * @return Devuelve el c�digo de un producto 
+	 */
+	public int getCodigo() {
+		return 0;
+	}
+	
+	/****
+	 * Devuelve el precio de un producto
+	 * @return Devuelve el precio de un producto 
+	 */
+	public double getPrecio() {
+		return 0.00;
+	}
+	
+	/****
+	 * Establece el precio de un producto
+	 * @param precio - Precio del producto 
+	 */
+	public void setPrecio(double precio) {
+		
+	}
+
+	/****
+	 * Devuelve la descripci�n de un producto
+	 * @return Devuelve la descripci�n de un producto 
+	 */
+	public String getDescripcion() {
+		return "";
+	}
+	
+	/****
+	 * Establece la descripcion de un producto
+	 * @param descripcion - Descripci�n del producto
+	 */
+	public void setDescripcion(String descripcion) {
+		
+	}
+	
+	/****
+	 * Devuelve informaci�n descripctiva del producto 
+	 * @return String que describa el estado del objeto
+	 */
+	public String toString() {
+		return "";
+	}
+}
---
+++ src/ar/edu/unlam/dominio/Venta.java
@@ -0,0 +1,78 @@
+package ar.edu.unlam.dominio;
+
+public class Venta {
+
+	/****
+	 * Constructor de la clase
+	 * @param eMail eMail del Ventador
+	 * @param cuil del Ventador
+	 * @param codigoQR de la Venta
+	 * @param porcentajeDescuento Determina el porcentaje de descuento asociado a la Venta. Su valor est� dado entre 0 y 1
+	 */
+	
+	private String eMail;
+	private String cuil;
+	private long codigoQR;
+	private double porcentajeDescuento;
+	
+	
+	
+	public Venta(String eMail, String cuil, long codigoQR, double porcentajeDescuento) {
+		this.eMail = eMail;
+		this.cuil = cuil;
+		this.codigoQR = codigoQR;
+		this.porcentajeDescuento = porcentajeDescuento;
+		
+	}
+
+	
+	/****
+	 * Devuelve el c�digo QR asociado a la Venta
+	 * @return Devuelve el c�digo QR asociado a la Venta
+	 */
+	public long getCodigoQR() {
+		return 0;
+	}
+
+	/****
+	 * Devuelve el E-Mail del Ventador
+	 * @param eMail
+	 */
+	public String getEMailVentador() {
+		return "";
+	}
+
+	/****
+	 * Devuelve el CUIL del Ventador
+	 * @param cuil
+	 */
+	public String getCuilVentador() {
+		return ""; 
+	}
+
+	/****
+	 * Agrega un nuevo producto a la Venta
+	 * @param nuevo Producto
+	 * @return true si se pudo agregar o false en caso contrario
+	 */
+	public boolean agregarProducto(Producto nuevo) {
+		return false;
+	}
+	
+	/****
+	 * Calcula el importe total de la Venta, el cual est� dado por la sumatoria de los importes de los productos incluidos, menos el porcentaje de descuento
+	 * @return Devuelve el importe total de la Venta
+	 */
+	public double getImporteTotal() {
+		return 0.00;
+	}
+	
+	/****
+	 * Devuelve informaci�n descripctiva de la Venta 
+	 * @return String que describa el estado del objeto
+	 */
+	public String toString() {
+		return "";
+	}
+
+}
